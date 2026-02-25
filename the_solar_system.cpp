728a6b1643299218dd19790058a893520d1a08b3
Reed Forman <reed.forman4@gmail.com>
2026-02-24 21:27:08 -0800

    Added comments and first namespace

--- the_solar_system.cpp
+++ the_solar_system.cpp
@@ -1,8 +1,22 @@
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+/****  New Workflow ****
+cd build
+cmake ..    <- only needed if CMakeLists changes
+make
+./solar
+
+*/
+
 
 int main()
 {
+
+    std::cout << "Solar System Simulation\n";
+
     float radius;
     scanf("%f",&radius);
     printf("The solar system had a radius of %.1f AU.\n", radius);
