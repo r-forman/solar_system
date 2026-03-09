b04187a8cd09b839e855a059a5d4cee89fb090b9
Reed Forman <reed.forman4@gmail.com>
2026-03-08 22:05:11 -0700

    Added better orbital pic and validation of calc

--- the_solar_system.cpp
+++ the_solar_system.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <matplot/matplot.h>
 #include <utility> // for std:pair
 
-/****  New Workflow ****
+/*      New Workflow
 cd build
 cmake ..    <- only needed if CMakeLists changes
 make
@@ -62,20 +63,17 @@ int main()
     std::cout << "Solar System Simulation\n";
 
     /*
-    double radius;
-    std::cout   << "Enter solar system radius [AU]: ";
-    std::cin    >> radius;
-    std::cout   << "The solar system had a radius of "
-                << radius
-                << std::endl;
+    double date;
+    std::cout   << "Enter the date desired: ";
+    std::cin    >> date;
     */
 
     auto [x, y] = calculate_position();
 
     std::cout   << "Planet position: " << x << ", " << y << std::endl;
 
-    std::cout << "*****************************" << std::endl;
-    std::cout << "*****************************" << std::endl;
+    std::cout << "                            *" << std::endl;
+    std::cout << "                            *" << std::endl;
 
     // Earth example (approx)
     double radius = 1.0;                 // AU
@@ -84,66 +82,50 @@ int main()
 
     Planet earth(radius, omega, 0.0, 0.0);
 
+
+
     double t = 100.0; // days
 
     orbital_pos pos = earth.position(t);
 
-    std::cout << "x = " << pos.x
-              << ", y = " << pos.y << "\n"
-              << "         -      -        ---            ---        -     \n"
-              << "        -     -      --                      --      -   \n"
-              << "      -     --     -        ------------        -     -- \n"
-              << "     -     -     -      --                --      -     -\n"
-              << "    -     -     -     -       -------        -     -     \n"
-              << "    -    -    --    --      -------------     --     --  \n"
-              << "   -    -     -    -     --                     -     -  \n"
-              << "   -    -    -    -     -                                \n"
-              << "  -    -    -    --    -     .-- ‾‾‾‾‾ --.    .          \n"
-              << "  -    -    -    -    -    /   .- ‾‾‾ -.   \\   -        \n"
-              << " -    -    -    -    -    |   |    O    |   |     -    - \n"
-              << "  -    -    -    -    -    \\   -. ___ .-   /  -     -     - \n"
-              << "  -    -    -    --         ` -- _____ -- '  .    -    --- \n"
-              << "   -    -    -    -     -                  -     -    -   -  \n"
-              << "   -    -     -    -     -                -     -    -     -  \n"
-              << "    -    -    --    --     --          --     --    --    -   \n"
-              << "    -     -     -     -                      -     -     -    \n"
-              << "-    -     -     -      --                --      -     -     \n"
-              << " -    -     --     -        ------------        -     --      \n"
-              << "  -     -     -      --                      --      -        \n";
     /*
-    // Circle
-    const int width = 100;
-    const int height = 20;
 
-    const double aspect = 2.0; // compensate for terminal character height
-
-    // Orbit size
-    double orbits[] = {4, 10, 16, 26};
-
-    for (int y = height / 2; y > -height / 2; --y) {
-        for (int x = -width / 2; x < width / 2; ++x) {
-
-            bool printed = false;
-
-            for (double a : orbits) {
-                double b = a * 0.6;  // flatten to make oval-ish
-
-                double eq =
-                    (x * x) / (a * a) +
-                    (y * y) / (b * b * aspect);
-
-                if (std::abs(eq - 1.0) < 0.05) {
-                    std::cout << "*";
-                    printed = true;
-                    break;
-                }
-            }
-
-            if (!printed)
-                std::cout << " ";
-        }
-        std::cout << "\n";
-    }
+    Celestial
+    Body        | Symbol
+    ---------   | ------
+    Sun	        | ☉
+    Mercury     | ☿
+    Venus       | ♀
+    Earth       | ⊕ or ♁
+    Moon        | ☾ or ☽
+    Mars        | ♂
+    Jupiter     | ♃
+    Saturn      | ♄
+    Uranus      | ♅ or ⛢|
+    Neptune     | ♆
+    Pluto       | ♇
     */
+
+    std::cout << "x = " << pos.x << ", y = " << pos.y << "\n"
+              << "   -      -        ---            ---        -     \n"
+              << "  -     -      --                      --      -   \n"
+              << "-     --     -        ------------        -     -- \n"
+              << "     -     -      --                --      -     -\n"
+              << "    -     -                            -     -     \n"
+              << "   -     -               _ _ _ _ _                 \n"
+              << "  -     -           - ‾‾           ‾‾ -            \n"
+              << "  -    -         -                       -         \n"
+              << " -    -        -       .-- ‾‾‾‾‾ --.       -       \n"
+              << " -    -       -      /   .- ‾‾‾ -.   \\      -     \n"
+              << "-    -        -     |   |    ☉    |   |     -      \n"
+              << " -    -       -      \\   -. ___ .-   /      -     \n"
+              << " -    -        -      ` -- _____ -- '      -       \n"
+              << "  -     -    -    -                     -          \n"
+              << "   -    --    --    ‾  - - - - - - -  ‾            \n"
+              << "    -     -     -                                  \n"
+              << "     -     -      --                    --         \n"
+              << "-     --     -        ----------------             \n"
+              << "  -     -      --                                  \n";
+
     return 0;
 }
